e9b9a876ae52d71377ecd82e5aa8c9aa10e0e38d
aladdindridi <alaeddine.dridi@outlook.com>
2025-08-12 11:48:59 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
     
   </React.StrictMode>,
   document.getElementById('root')
