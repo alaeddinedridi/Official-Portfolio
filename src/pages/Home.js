961b460bdd952055ada5969f1f3047738df1c083
aladdindridi <alaeddine.dridi@outlook.com>
2025-08-14 20:20:07 +0100

    added terminal to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,9 +6,30 @@ import {education} from '../data/education'
 import {experience} from '../data/experience'
 import {projects} from '../data/projects'
 import {certificates} from '../data/certificates'
+import Terminal from 'terminal-in-react';
+import { useEffect } from "react"
 
 
 const Home = () => {
+
+    const showMsg = () => 'Hello World'
+
+    var i = 0;
+    var txt = 'Lorem ipsum typing effect!'; /* The text */
+    var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+    const typeWriter=() => {
+        if (i < txt.length) {
+            document.getElementById("typing_text").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+
+    useEffect(() => {
+        typeWriter()
+    })
+
     return (
         <div>
             <header>
@@ -16,10 +37,17 @@ const Home = () => {
             </header>
             <main>
                 <section className={classes.home}>
-                    <div className={classes.picture}><img src={`${process.env.PUBLIC_URL}/images/cv picture.png`} /></div>
-                    <h1 id="home">Aladdin Dridi</h1>
-                    <h2>QA Engineer</h2>
-                    <div>Passionate about technology since a very young age. I deeply believe that we should never stop learning and never settle for anything less than being the best at what we do.</div>
+                    <div className={classes.info}>
+                        <div className={classes.picture}><img src={`${process.env.PUBLIC_URL}/images/cv picture.png`} /></div>
+                        <h1 id="home">Aladdin Dridi</h1>
+                        <h2>QA Engineer</h2>
+                        <div>Passionate about technology since a very young age. I deeply believe that we should never stop learning and never settle for anything less than being the best at what we do.</div>
+                    </div>
+                    <div className={classes.terminal}>
+                        <p id="typing_text">aladdin@home:~$ </p>
+                        <span id="myspan"></span>
+                    </div>
+                    
                 </section>
                 <section className={classes.projects}>
                     <h2 id="projects">Projects</h2>
